cb09766fb53f96b1bd906a3018a614eb58b2b597
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-11-26 15:49:08 -0500

    Committing old repo

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.NumWordsTypedLabel = new System.Windows.Forms.Label();
+            this.NumWordsTypedTextBox = new System.Windows.Forms.TextBox();
+            this.GradeLabel = new System.Windows.Forms.Label();
+            this.GradeResultLabel = new System.Windows.Forms.Label();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SubmitBtn.Location = new System.Drawing.Point(217, 97);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(75, 23);
+            this.SubmitBtn.TabIndex = 1;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // NumWordsTypedLabel
+            // 
+            this.NumWordsTypedLabel.AutoSize = true;
+            this.NumWordsTypedLabel.Location = new System.Drawing.Point(12, 15);
+            this.NumWordsTypedLabel.Name = "NumWordsTypedLabel";
+            this.NumWordsTypedLabel.Size = new System.Drawing.Size(126, 13);
+            this.NumWordsTypedLabel.TabIndex = 4;
+            this.NumWordsTypedLabel.Text = "Number of Words Typed:";
+            // 
+            // NumWordsTypedTextBox
+            // 
+            this.NumWordsTypedTextBox.Location = new System.Drawing.Point(144, 12);
+            this.NumWordsTypedTextBox.Name = "NumWordsTypedTextBox";
+            this.NumWordsTypedTextBox.Size = new System.Drawing.Size(148, 20);
+            this.NumWordsTypedTextBox.TabIndex = 0;
+            // 
+            // GradeLabel
+            // 
+            this.GradeLabel.AutoSize = true;
+            this.GradeLabel.Location = new System.Drawing.Point(99, 35);
+            this.GradeLabel.Name = "GradeLabel";
+            this.GradeLabel.Size = new System.Drawing.Size(39, 13);
+            this.GradeLabel.TabIndex = 5;
+            this.GradeLabel.Text = "Grade:";
+            // 
+            // GradeResultLabel
+            // 
+            this.GradeResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GradeResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GradeResultLabel.Location = new System.Drawing.Point(144, 35);
+            this.GradeResultLabel.Name = "GradeResultLabel";
+            this.GradeResultLabel.Size = new System.Drawing.Size(148, 42);
+            this.GradeResultLabel.TabIndex = 3;
+            this.GradeResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ClearBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ClearBtn.Location = new System.Drawing.Point(12, 97);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearBtn.TabIndex = 2;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.SubmitBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ClearBtn;
+            this.ClientSize = new System.Drawing.Size(304, 132);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.GradeResultLabel);
+            this.Controls.Add(this.GradeLabel);
+            this.Controls.Add(this.NumWordsTypedTextBox);
+            this.Controls.Add(this.NumWordsTypedLabel);
+            this.Controls.Add(this.SubmitBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lab 6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.Label NumWordsTypedLabel;
+        private System.Windows.Forms.TextBox NumWordsTypedTextBox;
+        private System.Windows.Forms.Label GradeLabel;
+        private System.Windows.Forms.Label GradeResultLabel;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,57 @@
+﻿/**
+ *  Grading ID:  L8486
+ *  Due date:    10/27/19
+ *  Class:       CIS 199-75
+ *  Lab:         6
+ *  Description: Loops though parallel arrays containing ranges for grade letter.
+ **/
+
+using System;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // (Cancel button) Clears all results from submit button
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            NumWordsTypedTextBox.Text = "";
+            GradeResultLabel.Text = "";
+            NumWordsTypedTextBox.Select();  // Select textbox as insertion point
+        }
+
+        // (Accept button) Loops though parallel arrays containing ranges for grade letter
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            char gradeLetter = '\x00'; // Grade letter for result label. Placeholder: Null byte char
+            char[] gradeLetterArray     = { 'A', 'B', 'C', 'D', 'F' }; // Array[4] PARALLEL: Array containing grade letters
+            int[] wordsTypedRangeArray  = {  76,  51,  31,  16,  0 };    // Array[4] PARALLEL: Array containing maximum range value for associated grade letter
+            //                 Array index: [0]  [1]  [2]  [3]  [4]
+            bool valid = false;     // Valid flag to escape for loop on discovery
+            if (int.TryParse(NumWordsTypedTextBox.Text, out int wordsTyped) && wordsTyped >= 0) // wordsTyped.Range: [0, infinity)
+            {
+                for (int x = 0; x <= wordsTypedRangeArray.Length - 1 && !valid; ++x)  // Start 0, go up until 4 or valid == true
+                {
+                    if (wordsTyped >= wordsTypedRangeArray[x])  // If found match
+                    {
+                        valid = true;                           // Set flag to true
+                        gradeLetter = gradeLetterArray[x];      // Assign parallel grade letter
+                    }
+                }
+                if (valid)                                      // If match was found in sorting
+                    GradeResultLabel.Text = $"{gradeLetter}";   // Assign grade letter to label text
+                // No else needed. Valid result range is [0, infinity).
+            }
+            else       // If invalid input, display error message
+            {
+                MessageBox.Show("Error! Words typed amount invalid.");
+            }
+        }
+    }
+}
